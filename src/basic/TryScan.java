da7f302b099920d312179220dbfe2af9eccdd2e1
chenboshuo <chenboshuo.hi@163.com>
2020-09-23 08:46:25 +0800

    :beers: try the input.

---
+++ src/basic/TryScan.java
@@ -0,0 +1,12 @@
+/**
+ * try the user input
+ */
+import java.util.Scanner;
+public class TryScan{
+  public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int x ;
+    x = sc.nextInt();
+    System.out.println(x);
+  }
+}
