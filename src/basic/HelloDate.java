964ae13a0fab9b4132983eb1edd4f492b51148e1
chenboshuo <chenboshuo.hi@163.com>
2020-10-14 06:39:53 +0800

    :speech_balloon: add the comments.

--- src/basic/HelloDate.java
+++ src/basic/HelloDate.java
@@ -1,9 +1,18 @@
 // there's a certain library of classes 
 // that are automatically brought into every java file:java.lang
 import java.util.*; // jvava.utl.Date
-
+/** The first Thinking in Java example program.
+ * Displays a string and today's date.
+ * @author Bruce Eckel
+ * @author www.MindView.net
+ * @version 4.0
+ * 
+ */
 public class HelloDate {
-
+  /** Entry point to class & application
+   * @param args array of string arguments
+   * @throws exceptions No exceptions thrown 
+   */
   public static void main(String[] args) {
     System.out.println("Hello it's");
     System.out.println(new Date());
@@ -19,3 +28,7 @@ public class HelloDate {
      */
   }
 }
+/* Output (55% match)
+Hello it's
+Wed Oct 14 06:37:12 CST 2020
+*/
